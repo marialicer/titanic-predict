b618796d81b443fbaf714d70911fef6cfd4f71ec
Alice Rocha <alicesouzarocha18@gmail.com>
2026-03-10 18:54:05 -0300

    mudanças no treino

--- train.py
+++ train.py
@@ -40,7 +40,8 @@ X_train, X_test, y_train, y_test = train_test_split (
     X, y, test_size=0.2, random_state=42
 )
 # %%
-model = DecisionTreeClassifier(max_depth=4)
+model = DecisionTreeClassifier(
+    max_depth=4)
 # %%
 model.fit(X_train,y_train)
 # %%
